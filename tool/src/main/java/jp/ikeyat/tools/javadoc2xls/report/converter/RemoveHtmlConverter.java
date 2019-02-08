29a23abac04fca43fda9b55a003eb906f0beb4ff
ikeyat <tm.ikeya@gmail.com>
2019-02-09 03:26:17 +0900

    Add cap <br> to convert #24
    (cherry picked from commit 5b31fd6362d86ad6cd5057a36a884821fa84e643)

--- tool/src/main/java/jp/ikeyat/tools/javadoc2xls/report/converter/RemoveHtmlConverter.java
+++ tool/src/main/java/jp/ikeyat/tools/javadoc2xls/report/converter/RemoveHtmlConverter.java
@@ -4,6 +4,8 @@ public class RemoveHtmlConverter implements Converter {
     public String convert(String source) {
         source = source.replaceAll("<br>", "\r\n");
         source = source.replaceAll("<br */>", "\r\n");
+        source = source.replaceAll("<BR>", "\r\n");
+        source = source.replaceAll("<BR */>", "\r\n");
         source = source.replaceAll("<[^<>]*>", "");
         return source;
     }
