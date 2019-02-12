353de70f1084e45f4105e6dd33cf2670f4f38ada
ikeyat <tm.ikeya@gmail.com>
2019-02-13 03:00:47 +0900

    Add null check in RemoveHtmlConverter #36
    (cherry picked from commit be362ffd873e242864c32255d88ca1df1ce4a833)

--- tool/src/test/java/jp/ikeyat/tools/javadoc2xls/report/converter/RemoveHtmlConverterTest.java
+++ tool/src/test/java/jp/ikeyat/tools/javadoc2xls/report/converter/RemoveHtmlConverterTest.java
@@ -3,6 +3,7 @@ package jp.ikeyat.tools.javadoc2xls.report.converter;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RemoveHtmlConverterTest {
@@ -48,11 +49,17 @@ public class RemoveHtmlConverterTest {
         assertThat(result, is("hoge\r\nfuga"));
     }
 
-
     @Test
     public void testTagRemoved_001() {
         Converter converter = new RemoveHtmlConverter();
         String result = converter.convert("<h1>hoge</h1><b>fuga</b>");
         assertThat(result, is("hogefuga"));
+    }
+
+    @Test
+    public void testNull_001() {
+        Converter converter = new RemoveHtmlConverter();
+        String result = converter.convert(null);
+        assertThat(result, is(nullValue()));
     }
 }
